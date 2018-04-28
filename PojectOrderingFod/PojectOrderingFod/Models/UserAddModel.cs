c76f5f6ea9c64d33e9140d89ae669fa8d2989fa8
AdrianS95 <adrian.smaza2@gmail.com>
2018-04-28 09:20:24 +0200

    Add User models and Registred controler

---
+++ PojectOrderingFod/PojectOrderingFod/Models/UserAddModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PojectOrderingFod.Models
+{
+    public class UserAddModel
+    {
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Telefon { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Adres { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string PoctCode { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Password { get; set; }
+
+    }
+}
