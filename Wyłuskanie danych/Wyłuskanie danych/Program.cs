a2474a752d975b564ff539be71ca85c2c0faa5a8
AdrianS95 <adrian.smaza2@gmail.com>
2018-05-03 12:07:49 +0200

    Add

--- Wyłuskanie danych/Wyłuskanie danych/Program.cs
+++ Wyłuskanie danych/Wyłuskanie danych/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,18 +10,106 @@ namespace Wyłuskanie_danych
 {
     class Program
     {
+        public static void f1()
+        {
+            var url = "http://www.betexplorer.com/soccer/poland/ekstraklasa/results/";
+
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var Bet = new List<Bet>();
+
+
+
+            // Lettura delle righe
+            var Rows = doc.DocumentNode.SelectNodes("//table");
+
+            foreach (var row in Rows)
+            {
+                if (!row.GetAttributeValue("class", "").Contains("table-main js-tablebanner-t js-tablebanner-ntb"))
+                {
+                    if (string.IsNullOrEmpty(row.InnerText))
+                        continue;
+
+                    var rowBet = new Bet();
+                    foreach (var node in row.ChildNodes)
+                    {
+                        var data_odd = node.GetAttributeValue("data-odd", "");
+
+                        if (string.IsNullOrEmpty(data_odd))
+                        {
+                            if (node.GetAttributeValue("class", "").Contains("in-match"))
+                            {
+                                rowBet.Match = node.InnerText.Trim();
+                                var matchTeam = rowBet.Match.Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+                                rowBet.Home = matchTeam[0];
+                                rowBet.Host = matchTeam[1];
+                            }
+
+
+                            if (node.GetAttributeValue("class", "").Contains("h-text-center"))
+                            {
+                                rowBet.Result = node.InnerText.Trim();
+                                var matchPoints = rowBet.Result.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                                int help;
+                                if (int.TryParse(matchPoints[0], out help))
+                                {
+                                    rowBet.HomePoints = help;
+                                }
+                                if (matchPoints.Length == 2 && int.TryParse(matchPoints[1], out help))
+                                {
+                                    rowBet.HostPoints = help;
+                                }
+
+                            }
+
+
+                            if (node.GetAttributeValue("class", "").Contains("h-text-right h-text-no-wrap"))
+                                rowBet.Date = node.InnerText.Trim();
+
+                        }
+                        else
+                        {
+                            rowBet.Odds.Add(data_odd);
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(rowBet.Match))
+                        Bet.Add(rowBet);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            //var url = "http://html-agility-pack.net/";
+
             var url = "https://www.pyszne.pl/restauracja-34-120";
             var web = new HtmlWeb();
             var doc = web.Load(url);
 
-            Console.WriteLine(doc.Text);
+            var html =
+         @"<TD class=texte width=""50%"">
+			<DIV align=right>Name :<B> </B></DIV>
+		</TD>
+		<TD width=""50%"">
+    		<INPUT class=box value=John maxLength=16 size=16 name=user_name>
+    		<INPUT class=box value=Tony maxLength=16 size=16 name=user_name>
+    		<INPUT class=box value=Jams maxLength=16 size=16 name=user_name>
+		</TD>
+		<TR vAlign=center>";
 
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(doc.Text);
 
+            var htmlNodes = htmlDoc.DocumentNode.SelectNodes("div class='restaurant grid'");
+            //var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//td/input");
 
+            foreach (var node in htmlNodes)
+            {
 
-
+                Console.WriteLine(node.Attributes["value"].Value);
+            }
 
             Console.ReadKey();
         }
